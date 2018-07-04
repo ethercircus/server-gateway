798ae0d951c0be666f61b8e5c49b43f3d67e70a1
cameronvoell <cameronvoell@gmail.com>
2018-07-03 19:44:29 -0700

    Updated to new publication register contract event, and updated vendor reference to ethercircus solidity-contracts code generated golang file

--- ethercircus.go
+++ ethercircus.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/ethercircus/server-gateway/contracts"
+	"github.com/ethercircus/solidity-contracts/contracts/gocontracts"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/log"
 )
@@ -28,21 +28,21 @@ func main() {
 		log.Crit("Failed to connect to the Ethereum client: %v", err)
 	}
 	// Instantiate the contract and display its name
-	userContentRegister, err := contracts.NewUserContentRegister(common.HexToAddress("0x8979BDf9A55C8B5a8ce4b30c4D80D678b8a91800"), conn)
+	publicationRegister, err := gocontracts.NewPublicationRegister(common.HexToAddress("0x18BC6842b400b09D5CC06aBfB7522a12d8F23579"), conn)
 	if err != nil {
 		log.Crit("Failed to instantiate the UserContentRegister contract: %v", err)
 	}
-	ret, err := userContentRegister.UserIndex(nil, common.HexToAddress("0xe2eb4e5418e8d1f90b474318b83034a15fae409f"))
-	if err != nil {
-		log.Crit("Failed to retrieve username: %v", err)
-	}
-	fmt.Println("User name: ", ret.UserName)
+	//ret, err := userContentRegister.UserIndex(nil, common.HexToAddress("0xe2eb4e5418e8d1f90b474318b83034a15fae409f"))
+	//if err != nil {
+	//	log.Crit("Failed to retrieve username: %v", err)
+	//}
+	//fmt.Println("User name: ", ret.UserName)
 	var blockNumber uint64 = 2530622
 
-	ch := make(chan *contracts.UserContentRegisterStoreData)
+	ch := make(chan *gocontracts.PublicationRegisterStoreData)
 	opts := &bind.WatchOpts{}
 	opts.Start = &blockNumber
-	storeDataSub, err := userContentRegister.WatchStoreData(opts, ch)
+	storeDataSub, err := publicationRegister.WatchStoreData(opts, ch)
 	if err != nil {
 		log.Crit("Failed WatchStoreData: %v", err)
 	}
@@ -52,13 +52,13 @@ func main() {
 
 		for {
 			//received an Ethereum Contract Event
-			var newEvent *contracts.UserContentRegisterStoreData = <-ch
+			var newEvent *gocontracts.PublicationRegisterStoreData = <-ch
 			fmt.Println(newEvent.Data)
 			go func() {
-				ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+				ctx, cancel := context.WithTimeout(context.Background(), 25*time.Second)
 				defer cancel()
-				_, error := ipfs.Content(ctx, newEvent.Data)
-				if error == nil {
+				_, err := ipfs.Content(ctx, newEvent.Data)
+				if err == nil {
 					fmt.Println("this shit may have actually worked")
 				} else {
 					fmt.Println("Error ipfs.Content command: " + err.Error())
